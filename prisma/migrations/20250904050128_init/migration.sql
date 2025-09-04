be2977797287a9ee3683c020c4845fb406564deb
abdelhamid <abdelhamidgoulahssen@gmail.com>
2025-09-04 07:12:18 +0100

    Add Prisma schema and migrations

---
+++ prisma/migrations/20250904050128_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `author` VARCHAR(191) NOT NULL,
+    `ISBN` VARCHAR(191) NOT NULL,
+    `category` ENUM('FICTION', 'NONFICTION', 'HISTORY', 'SCIENCE') NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `price` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
